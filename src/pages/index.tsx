2c11d9c51d34c49c19ef0687e3ecdb08a75a7d03
Matheus G. Rodrigues <matheusgomes1203@hotmail.com>
2024-06-23 16:10:18 -0300

    Adicionando funcionalidade de download cv

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 
@@ -8,12 +8,28 @@ import Link from '@docusaurus/Link';
 export default function Home(): React.ReactElement {
     const { siteConfig } = useDocusaurusContext();
 
+    const handleDownloadCV = useCallback(() => {
+        const fileName = 'curriculo-matheus-gomes-rodrigues-de-jesus';
+
+        const pdfUrl = `/img/${fileName}.pdf`;
+
+        const link = document.createElement('a');
+        link.href = pdfUrl;
+        link.download = fileName;
+
+        document.body.appendChild(link);
+
+        link.click();
+
+        document.body.removeChild(link);
+    }, []);
+
     return (
         <Layout description="Descrição dos meus processos de trabalho" title={siteConfig.title}>
             <section className="max-w-2xl container items-center flex-col flex mt-10 lg:mt-16">
                 <div className="items-center flex-col w-max flex gap-3">
                     <div className="overflow-hidden border-slate-200 border-solid rounded-full bg-slate-400 border-3 w-20 h-20">
-                        <img className="w-full h-full" src="/img/foto.jpeg" />
+                        <img className="w-full h-full" src="/img/foto.jpeg" alt={`${siteConfig.tagline}`} />
                     </div>
                     <p className="text-sm">{siteConfig.title}</p>
                 </div>
@@ -33,7 +49,10 @@ export default function Home(): React.ReactElement {
                 </Link>
 
                 <div className="gap-4 flex mt-4">
-                    <button className="text-slate-900 dark:text-white bg-transparent font-semibold cursor-pointer border-none text-sm">
+                    <button
+                        onClick={handleDownloadCV}
+                        className="text-slate-900 dark:text-white bg-transparent font-semibold cursor-pointer border-none text-sm"
+                    >
                         Download CV
                     </button>
                     <button className="text-white bg-gradient-to-r from-purple-900 to-purple-500 hover:opacity-90 cursor-pointer font-semibold rounded-lg border-none text-sm w-40 py-4">
