fa78f01809d0d14574667e74eb556459d8d20705
Matheus G. Rodrigues <matheusgomes1203@hotmail.com>
2024-06-23 12:30:58 -0300

    Configurando

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,45 +1,67 @@
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 
-import Layout from "@theme/Layout";
-import Link from "@docusaurus/Link";
-import { useRef } from "react";
+import Layout from '@theme/Layout';
+import Link from '@docusaurus/Link';
+import { ImgHTMLAttributes, useRef } from 'react';
 
 interface ButtonLink {
-   children: React.ReactNode;
-   to: string;
+    children: React.ReactNode;
+    to: string;
 }
 
 const ButtonLink: React.FC<ButtonLink> = ({ children, to }) => (
-   <Link
-      className="bg-gradient-to-b from-purple-700 to-purple-500 text-white button hover:text-white shadow--tl rounded-lg text-md1 w-auto py-3 px-6 mt-4"
-      to={to}
-   >
-      {children}
-   </Link>
+    <Link
+        className="bg-gradient-to-b from-purple-700 to-purple-500 text-white button hover:text-white shadow--tl rounded-lg text-md1 w-auto py-3 px-6 mt-4"
+        to={to}
+    >
+        {children}
+    </Link>
 );
 
+interface AvatarProps extends React.DetailedHTMLProps<ImgHTMLAttributes<HTMLImageElement>, HTMLImageElement> {}
+
+const Avatar: React.FC<AvatarProps> = ({ ...props }) => {
+    return (
+        <div className="">
+            <img {...props} />
+        </div>
+    );
+};
+
 export default function Home(): React.ReactElement {
-   const { siteConfig } = useDocusaurusContext();
+    const { siteConfig } = useDocusaurusContext();
 
-   return (
-      <Layout description="Descrição dos meus processos de trabalho" title={`${siteConfig.title} - Processos`}>
-         <header className=" padding--lg hero">
-            <section className="container text-center md:my-[16vh]">
-               <div className=" avatar avatar--vertical">
-                  <img className="shadow--tl avatar__photo avatar__photo--xl" src="/img/foto.jpeg" />
-                  <div className="avatar__intro">
-                     <div className="avatar__name font-sans mt-2">GomesDev</div>
-                     <small className="avatar__subtitle">Frontend Developer</small>
-                  </div>
-               </div>
-
-               <span className="badge badge--secondary my-4">Este projeto está em andamento</span>
-
-               <h3 className="my-2">{siteConfig.tagline}</h3>
-
-               <ButtonLink to="/docs/visao-geral">Começar</ButtonLink>
-            </section>
-         </header>
-      </Layout>
-   );
+    return (
+        <Layout description="Descrição dos meus processos de trabalho" title={siteConfig.title}>
+            <Avatar />
+        </Layout>
+    );
 }
+
+/*
+export default function Home(): React.ReactElement {
+    const { siteConfig } = useDocusaurusContext();
+
+    return (
+        <Layout description="Descrição dos meus processos de trabalho" title={`${siteConfig.title} - Processos`}>
+            <header className=" padding--lg hero">
+                <section className="container text-center md:my-[16vh]">
+                    <div className=" avatar avatar--vertical">
+                        <img className="shadow--tl avatar__photo avatar__photo--xl" src="/img/foto.jpeg" />
+                        <div className="avatar__intro">
+                            <div className="avatar__name font-sans mt-2">GomesDev</div>
+                            <small className="avatar__subtitle">Frontend Developer</small>
+                        </div>
+                    </div>
+
+                    <span className="badge badge--secondary my-4">Este projeto está em andamento</span>
+
+                    <h3 className="my-2">{siteConfig.tagline}</h3>
+
+                    <ButtonLink to="/docs/visao-geral">Começar</ButtonLink>
+                </section>
+            </header>
+        </Layout>
+    );
+}
+*/
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,44 +1,45 @@
+/* eslint-disable */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-   content: ["./src/**/*.{js,jsx,ts,tsx}"],
-   darkMode: ["class", '[data-theme="dark"]'],
-   corePlugins: {
-      preflight: false,
-   },
-   theme: {
-      extend: {
-         colors: {
-            "gray-900": "#0D0E0F",
-            "gray-600": "#282A2D",
-            "purple-300": "#AA88FF",
-            "purple-900": "#9B30F9",
-            "slate-300": "#B0B2A8",
-            "purple-500": "#8B42F0",
-            "purple-700": "#9B30F9",
-            orange: "#FF6622",
-            yellow: "#FFD300",
-            black: "#000000",
-            green: "#23D207",
-            white: "#FFFFFF",
-            blue: "#119DE6",
-            red: "#FF2800",
-         },
-         fontFamily: {
-            sans: ["Rubik", "sans-serif"],
-            serif: ["IBMPlexMono", "serif"],
-         },
-         fontSize: {
-            sm1: "12px",
-            sm2: "14px",
-            base: "16px",
-            md1: "18px",
-            md2: "24px",
-            lg1: "28px",
-            lg2: "48px",
-            xl1: "52px",
-            xl2: "56px",
-         },
-      },
-   },
-   plugins: [],
+    content: ['./src/**/*.{js,jsx,ts,tsx}'],
+    darkMode: ['class', '[data-theme="dark"]'],
+    corePlugins: {
+        preflight: false,
+    },
+    theme: {
+        extend: {
+            colors: {
+                'gray-900': '#0D0E0F',
+                'gray-600': '#282A2D',
+                'purple-300': '#AA88FF',
+                'purple-900': '#9B30F9',
+                'slate-300': '#B0B2A8',
+                'purple-500': '#8B42F0',
+                'purple-700': '#9B30F9',
+                orange: '#FF6622',
+                yellow: '#FFD300',
+                black: '#000000',
+                green: '#23D207',
+                white: '#FFFFFF',
+                blue: '#119DE6',
+                red: '#FF2800',
+            },
+            fontFamily: {
+                sans: ['Rubik', 'sans-serif'],
+                serif: ['IBMPlexMono', 'serif'],
+            },
+            fontSize: {
+                sm1: '12px',
+                sm2: '14px',
+                base: '16px',
+                md1: '18px',
+                md2: '24px',
+                lg1: '28px',
+                lg2: '48px',
+                xl1: '52px',
+                xl2: '56px',
+            },
+        },
+    },
+    plugins: [],
 };
